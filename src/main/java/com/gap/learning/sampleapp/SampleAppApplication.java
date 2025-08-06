7a0da9cca16cb4dab0671fc5b9f28e9a52796e51
Yogendra Singh <pratap.yogendra@gmail.com>
2025-08-06 16:42:35 +0530

    second commit

---
+++ src/main/java/com/gap/learning/sampleapp/SampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.gap.learning.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleAppApplication.class, args);
+    }
+
+}
